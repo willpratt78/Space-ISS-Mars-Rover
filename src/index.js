5622b2f1f634156e0a1bbd7283d5055a28ad11fd
William Pratt <willpratt78@gmail.com>
2022-04-26 14:11:42 -0700

    will cause all people currently on the ISS to be posted to the DOM

--- src/index.js
+++ src/index.js
@@ -1,16 +1,35 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { ISSTracker } from "./iss-tracker.js";
-// import { BingLocation } from "./bing-api.js";
+import { ISSTracker } from "./js/iss-tracker.js";
+// import { BingLocation } from "./js/bing-api.js";
 import $ from 'jquery';
-import { Distance } from './utility';
+import { Distance } from './js/utility.js';
+// import { Nasa } from './js/nasa.js'
+import {PeopleInSpace} from "./js/people.js";
+
 function displayLandmarks(landmarksArray) {
   const output = $("#landmark-output");
+  $("#landmark-output").text("");
   for (let i = 0; i < landmarksArray.length; i++) {
-    $(output).append(`<li>Landmark: ${landmarksArray[i][1]} Distance from ISS: ${landmarksArray[i][0]}km </li>`);
+    $(output).append(`<li> ${landmarksArray[i][1]} is: ${landmarksArray[i][0]}km away from the ISS!</li>`);
   }
 }
+
+async function displayPeople() {
+  const peopleArray = await PeopleInSpace.getPeopleSpace();
+  console.log(peopleArray)
+  const output = $("#output-people");
+  $("#output-people").text("");
+  for (let i = 0; i < peopleArray.length; i++) {
+    if(peopleArray[i].craft !== "ISS"){
+      return "";
+    }else
+    $(output).append(`<li> Right now ${peopleArray[i].name} is on the ${peopleArray[i].craft} !</li>`);
+  }
+}
+
+
 // function updateGoogleMaps(lat, long){
 //   const iframe = $("#google-maps-iframe");
 //   const src = `https://www.google.com/maps/embed/v1/view?key=${process.env.API_KEY_GOOGLE}
@@ -18,6 +37,7 @@ function displayLandmarks(landmarksArray) {
 //       &maptype=satellite`;
 //   $(iframe).attr("src", src);
 // }
+
 $("#object-location").submit(async function(event) {
   event.preventDefault();
   // let address = $('#address-input').val();
@@ -28,14 +48,11 @@ $("#object-location").submit(async function(event) {
   let distance = Distance.getDistanceFromLatLonInKm(location[0],location[1], userLocation[0], userLocation[1]);
   const landmarksArray = await ISSTracker.getLocationsOfLandmarksFromIss(location[0], location[1]);
   displayLandmarks(landmarksArray);
+  displayPeople();
   // updateGoogleMaps(location[0], location[1]);
   let displayDistance = (Math.round(distance * 10)/10).toLocaleString();
-  $("#output").text("you are " + displayDistance + " kilometers away from the ISS");
+  $("#output").text("you are " + displayDistance + " kilometers away from the ISS!");
 });
 
-// $("#wonders").submit(async function(event) {
-//   event.preventDefault();
 
-//   for (let index = 0; index < landmarksArray.length; index++) {
-//     const element = array[index];
-//   }   
+
